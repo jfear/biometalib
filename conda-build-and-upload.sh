e4e830b6b39a2a9ae2b87bba60729dc386834ade
Justin Fear <justin.m.fear@gmail.com>
2017-06-28 09:07:29 -0400

    oops had commented some things out.

--- conda-build-and-upload.sh
+++ conda-build-and-upload.sh
@@ -4,12 +4,12 @@ export BIOMETALIB_BUILD=$(git rev-list --count `git describe --tags --abbrev=0`.
 
 conda build conda-recipe
 
-#if [[ $TRAVIS_BRANCH = "master" && $TRAVIS_PULL_REQUEST = "false" ]]; then
-#  conda install anaconda-client -y
-#  anaconda \
-#    -t $ANACONDA_TOKEN \
-#    upload \
-#    -u jfear \
-#    --force \
-#    $(conda build --output conda-recipe)
-#fi
+if [[ $TRAVIS_BRANCH = "master" && $TRAVIS_PULL_REQUEST = "false" ]]; then
+  conda install anaconda-client -y
+  anaconda \
+    -t $ANACONDA_TOKEN \
+    upload \
+    -u jfear \
+    --force \
+    $(conda build --output conda-recipe)
+fi
